b5641abc3b7b0762ff0754d70732e347d118773e
HariBalaji96 <harianandbalaji@gmail.com>
2026-04-04 03:25:27 +0530

    Fix: Typo in FirebaseConfig package declaration preventing Bean Component Scan on Docker/Linux

--- src/main/java/com/example/BazaarTrackerBackend/config/FirebaseConfig.java
+++ src/main/java/com/example/BazaarTrackerBackend/config/FirebaseConfig.java
@@ -1,4 +1,4 @@
-package com.example.bazaarTrackerBackend.config;
+package com.example.BazaarTrackerBackend.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
